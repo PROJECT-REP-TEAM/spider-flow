baaa0205984835634e578dffa4b7636753a6b10d
xiangronghua <123456>
2020-12-04 10:23:13 +0800

    添加远程控制

--- spider-flow-core/src/main/java/org/spiderflow/core/executor/shape/ExecuteFlagExecutor.java
+++ spider-flow-core/src/main/java/org/spiderflow/core/executor/shape/ExecuteFlagExecutor.java
@@ -6,31 +6,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spiderflow.Grammerable;
 import org.spiderflow.context.SpiderContext;
-import org.spiderflow.core.io.HttpRequest;
-import org.spiderflow.core.io.HttpResponse;
 import org.spiderflow.core.mapper.NodeTaskStatusMapper;
 import org.spiderflow.core.model.NodeTaskStatus;
-import org.spiderflow.core.service.NodeTaskStatusService;
 import org.spiderflow.core.utils.DataSourceUtils;
-import org.spiderflow.core.utils.ExpressionUtils;
-import org.spiderflow.core.utils.ExtractUtils;
 import org.spiderflow.executor.ShapeExecutor;
 import org.spiderflow.model.Grammer;
 import org.spiderflow.model.SpiderNode;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.ArgumentPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.lang.reflect.Array;
-import java.net.URLEncoder;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.*;
 
 /**
@@ -39,7 +29,7 @@ import java.util.*;
  * @author jmxd
  */
 @Component
-public class ExecuteSQLFlagExecutor implements ShapeExecutor, Grammerable {
+public class ExecuteFlagExecutor implements ShapeExecutor, Grammerable {
 
     public static final String DATASOURCE_ID = "datasourceId";
 
@@ -47,30 +37,13 @@ public class ExecuteSQLFlagExecutor implements ShapeExecutor, Grammerable {
     public static final String PK_Name = "pkName";
     public static final String TABLE_Name = "tableName";
 
-    public static final String STATEMENT_TYPE = "statementType";
-
-    public static final String STATEMENT_SELECT = "select";
-
-    public static final String STATEMENT_SELECT_ONE = "selectOne";
-
-    public static final String STATEMENT_SELECT_INT = "selectInt";
-
-    public static final String STATEMENT_INSERT = "insert";
-
-    public static final String STATEMENT_UPDATE = "update";
-
-    public static final String STATEMENT_LAST_FLAG = "lastFLag";
-
-    public static final String STATEMENT_DELETE = "delete";
-    public static final String SELECT_RESULT_STREAM = "isStream";
-    public static final String STATEMENT_INSERT_PK = "insertofPk";
 
 
     @Autowired
     private NodeTaskStatusMapper nodeTaskStatusMapper;
 
 
-    private static final Logger logger = LoggerFactory.getLogger(ExecuteSQLFlagExecutor.class);
+    private static final Logger logger = LoggerFactory.getLogger(ExecuteFlagExecutor.class);
 
     @Override
     public void execute(SpiderNode node, SpiderContext context, Map<String, Object> variables) throws IOException {
@@ -199,15 +172,15 @@ public class ExecuteSQLFlagExecutor implements ShapeExecutor, Grammerable {
 
     @Override
     public String supportShape() {
-        return "executeSqlFlag";
+        return "executeFlag";
     }
 
     @Override
     public List<Grammer> grammers() {
         Grammer grammer = new Grammer();
-        grammer.setComment("执行SQL结果2");
+        grammer.setComment("顺序获取表数据");
         grammer.setFunction("rs");
-        grammer.setReturns(Arrays.asList("List<Map<String,Object>>", "int"));
+        grammer.setReturns(Arrays.asList("Map<String,Object>", "int"));
         return Collections.singletonList(grammer);
     }
 
